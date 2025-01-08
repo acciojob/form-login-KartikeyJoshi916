f0f510d26a0235b43e0f7af2039be1b9d85e8dba
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-08 17:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	let arr=document.getElementById("form1");
+	let fname=arr[0].value;
+	let lname=arr[1].value;
+	let message=fname+" "+lname;
+	alert(message);
 }
